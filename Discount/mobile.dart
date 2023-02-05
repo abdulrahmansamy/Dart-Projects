7af867a2318fbd9a4d8d2551638ced9c7c12c91b
abdulrahmansamy <abdul.m.samy@gmail.com>
2023-02-05 15:01:44 +0300

    mobile v2.1

--- Discount/mobile.dart
+++ Discount/mobile.dart
@@ -7,20 +7,14 @@ class mobile {
   int discount = 0;
   var mobileDetails = new Map();
   
-  // mobileDetails = {
-  //   "Brand": brand,
-  //   "Model": model,
-  //   "MRP": mrp,
-  //   "Discount": discount
-  // };
 mobile(int mrp1, int discount1,var brand1, var model1){
   mobileDetails["MRP"] = mrp = mrp1;
   mobileDetails["Discount"] = discount = discount1;
   mobileDetails["Brand"] = brand = brand1;
   mobileDetails["Model"] = model = model1;
 }
+
 double  getActualPrice(){
-  
   return (mrp-mrp*discount/100);
 }
 
