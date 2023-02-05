a379f5125f0d84126071716661e50be7ffb4d8cd
abdulrahmansamy <abdul.m.samy@gmail.com>
2023-02-05 14:35:15 +0300

    mobile v2.0

--- Discount/main.dart
+++ Discount/main.dart
@@ -3,7 +3,25 @@ import 'mobile.dart';
 void main(){
   print('Hello World');
   var m1 = mobile(1000, 10,"Nokia", "3310");
-  var discounted = m1.getActualPrice();
-  print("the discounted price: $discounted");
+
   m1.printDetails();
+  print("the discounted price: " + m1.getActualPrice().toString());
+  
+  
+
+  var m2 = mobile(10000, 10,"Samsung Galaxy", "S 20");
+
+  m2.printDetails();
+  print("the discounted price: " + m2.getActualPrice().toString());
+
+  var m3 = mobile(20000, 10,"IPhone", "11 Pro");
+
+  m3.printDetails();
+  print("the discounted price: " + m3.getActualPrice().toString());
+
+
+  var m4 = mobile(7000, 10,"One Plus", "7");
+
+  m4.printDetails();
+  print("the discounted price: " + m4.getActualPrice().toString());
 }
